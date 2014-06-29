bd15646591c2f1dbe5e308f7d583df9f5fddb177
Tim Wagner <t.wagner@techdivision.com>
2014-06-29 14:48:43 +0200

    Initial Release

---
+++ src/TechDivision/Properties/PropertyFileNotFoundException.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * \TechDivision\Properties\PropertyFileNotFoundException
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * PHP version 5
+ *
+ * @category  Library
+ * @package   TechDivision_Properties
+ * @author    Tim Wagner <tw@techdivision.com>
+ * @copyright 2014 TechDivision GmbH <info@techdivision.com>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      https://github.com/techdivision/TechDivision_Properties
+ */
+
+namespace TechDivision\Properties;
+
+/**
+ * This is the exception that is thrown if the specified file does not exist.
+ *
+ * @category  Library
+ * @package   TechDivision_Properties
+ * @author    Tim Wagner <tw@techdivision.com>
+ * @copyright 2014 TechDivision GmbH <info@techdivision.com>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      https://github.com/techdivision/TechDivision_Properties
+ */
+class PropertyFileNotFoundException extends \Exception
+{
+}
